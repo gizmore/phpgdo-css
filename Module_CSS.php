883296c6da8d8a010a7c1d4c5ac3f6bf49a7e8b4
gizmore <gizmore@wechall.net>
2022-05-22 22:52:03 +0200

    '7.0.0-r1342 RELEASE 7.0.0 Branch will be created.'

--- Module_CSS.php
+++ Module_CSS.php
@@ -8,7 +8,7 @@ use GDO\Core\GDT_Checkbox;
  * CSS related settings and toolchain utilities.
  * 
  * @author gizmore
- * @version 7.0.1
+ * @version 7.0.0
  * @since 6.10.5
  */
 final class Module_CSS extends GDO_Module
